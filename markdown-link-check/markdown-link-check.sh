ee26aa0bb4a9fbb091feb23b81f01bf41d69beaf
Tobias Gabriel <tobias.gabriel@sap.com>
2021-05-17 19:22:19 +0200

    blub

--- markdown-link-check/markdown-link-check.sh
+++ markdown-link-check/markdown-link-check.sh
@@ -22,10 +22,10 @@ while IFS= read -r -d $'\0' file; do
         while IFS= read -r link_error; do
             # echo "ERROR:$file:$link_error"
             # echo "$relative_file"
-            link_errors_for_annotation=$(printf "$link_errors_for_annotation$link_error%0A")
+            link_errors_for_annotation=$(printf "$link_errors_for_annotation$link_error %0A")
             # echo "::error file=$relative_file,line=0,col=0::A link in this file seems to be broken.%0A$link_error"
         done < <(echo "$link_check_errors")
-        echo "::error file=$relative_file,line=0,col=0::One or more links in this file seems to be broken.%0A$link_errors_for_annotation"
+        echo "::error file=$relative_file,line=0,col=0::One or more links in this file seems to be broken. %0A$link_errors_for_annotation"
 
      fi
 done < <(find $absolute_path -name '*.md' -print0) # use null seperator to allow for spaces in filename
