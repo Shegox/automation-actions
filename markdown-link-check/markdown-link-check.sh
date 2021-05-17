200cc84f31ea8ab1c4c1748b63968045cf33c9ee
Tobias Gabriel <tobias.gabriel@sap.com>
2021-05-17 19:20:01 +0200

    blub

--- markdown-link-check/markdown-link-check.sh
+++ markdown-link-check/markdown-link-check.sh
@@ -18,10 +18,11 @@ while IFS= read -r -d $'\0' file; do
         relative_file=$(realpath --relative-to="$base_path" "$file")
         # special output for problem matcher
         link_errors_for_annotation=""
+        echo "$relative_file"
         while IFS= read -r link_error; do
             # echo "ERROR:$file:$link_error"
-            echo "$relative_file"
-            link_errors_for_annotation=$(printf "$link_errors_for_annotation%0A$link_error")
+            # echo "$relative_file"
+            link_errors_for_annotation=$(printf "$link_errors_for_annotation$link_error%0A")
             # echo "::error file=$relative_file,line=0,col=0::A link in this file seems to be broken.%0A$link_error"
         done < <(echo "$link_check_errors")
         echo "::error file=$relative_file,line=0,col=0::One or more links in this file seems to be broken.%0A$link_errors_for_annotation"
